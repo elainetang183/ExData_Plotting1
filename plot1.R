f3c597b3afcf73792eb077aeeacfbc4d9e1faf22
Elaine Tang <54303824+elainetang183@users.noreply.github.com>
2019-11-22 11:12:52 +0800

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# Reading, naming and subsetting the data
+power<-read.table("./Exploratory Data Analysis Course Project 1/household_power_consumption.txt",skip=1,sep=";")
+names(power) <-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower<-subset(power,power$Date=="1/2/2007"|power$Date=="2/2/2007")
+
+# Calling the basic plot function and annotating the graph
+hist(as.numeric(as.character(subpower$Global_active_power)),col = "red",main = "Global Active Power",xlab = "Global Active Power (kilowatts)",ylab = "Frequency")
+
+# Create PNG file
+
+dev.copy(png,file="plot1.png")
+dev.off()
